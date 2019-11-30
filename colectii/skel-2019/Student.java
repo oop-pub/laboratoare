ec5030ff4e2735e42f8530ce8a0b65ae7cbecc11
Florin Mihalache <florinrm@users.noreply.github.com>
2019-11-30 20:05:57 +0200

    Added skel for lab 8 2019 (#9)

---
+++ colectii/skel-2019/Student.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.lang.*;
+
+public class Student {
+    private long id;
+    private String name;
+    private String surname;
+    private double averageGrade;
+
+    public Student(long id, String name, String surname, double averageGrade) {
+        this.id = id; 
+        this.name = name;
+        this.surname = surname;
+        this.averageGrade = averageGrade;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getAverageGrade() {
+        return this.averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+}
